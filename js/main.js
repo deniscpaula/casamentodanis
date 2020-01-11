df9c0e96d904509a47afa221396606e26887383a
Denis Campos <deniscpaula@hotmail.com>
2020-01-10 22:26:44 -0300

    Fix countdown safari

--- js/main.js
+++ js/main.js
@@ -255,39 +255,39 @@
 	
 
 	// Set the date we're counting down to
-		var countDownDate = new Date("2020-10-10 16:00").getTime();
+    var countDownDate = new Date(2020, 9, 10, 16, 0).getTime();
 
-		// Update the count down every 1 second
-		var x = setInterval(function() {
+    // Update the count down every 1 second
+    var x = setInterval(function() {
 
-		// Get todays date and time
-		var now = new Date().getTime();
+        // Get todays date and time
+        var now = new Date().getTime();
 
-		// Find the distance between now an the count down date
-		var distance = countDownDate - now;
+        // Find the distance between now an the count down date
+        var distance = countDownDate - now;
 
-		// Time calculations for days, hours, minutes and seconds
-		var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-		var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-		var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-		var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+        // Time calculations for days, hours, minutes and seconds
+        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-		// Display the result in an element with id="demo"
-		// document.getElementById("demo").innerHTML = days + "Days " + hours + "Hours "
-		// + minutes + "Minutes " + seconds + "Seconds ";
+        // Display the result in an element with id="demo"
+        // document.getElementById("demo").innerHTML = days + "Days " + hours + "Hours "
+        // + minutes + "Minutes " + seconds + "Seconds ";
 
-		// Display the result in an element with id="demo"
-		document.getElementById("days").innerHTML = days +" <small>Dias</small>";
-		document.getElementById("hours").innerHTML = hours + " <small>Horas</small> ";
-		document.getElementById("minutes").innerHTML = minutes + " <small>Minutos</small> ";
-		document.getElementById("seconds").innerHTML = seconds + " <small>Segundos</small> ";
+        // Display the result in an element with id="demo"
+        document.getElementById("days").innerHTML = days +" <small>Dias</small>";
+        document.getElementById("hours").innerHTML = hours + " <small>Horas</small> ";
+        document.getElementById("minutes").innerHTML = minutes + " <small>Minutos</small> ";
+        document.getElementById("seconds").innerHTML = seconds + " <small>Segundos</small> ";
 
-		// If the count down is finished, write some text 
-		if (distance < 0) {
-		 clearInterval(x);
-		 document.getElementById("demo").innerHTML = "The Wedding Ceremony is Over";
-		}
-		}, 1000);	
+        // If the count down is finished, write some text 
+        if (distance < 0) {
+            clearInterval(x);
+            document.getElementById("demo").innerHTML = "Casamento realizado e noivos felizes!";
+        }
+    }, 1000);	
 	
 		
 	var bgVideo = function() {
